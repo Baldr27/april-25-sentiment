31dfab48552961c43363c65429b7abbc368fc361
Belfor Acuña <bel4negro@gmail.com>
2024-04-25 11:44:39 -0400

    feature add link and color variants
    oli

--- sketch.js
+++ sketch.js
@@ -7,12 +7,12 @@ let prediction;
 let dibujar = true;
 let apiRequest;
 let comentarios = [];
-
-/* 
-*/
+let inputLink;
+let calcularButton;
+let comentariosAnalizados = [];
 
 const getData = async () => {
-  const apiRequest = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=Jn09UdSb3aA&key=AIzaSyDZ9MqeC6ebJXBJNY5X1kACBpHCmUaJtCM&order=relevance';
+  fillLink(inputLink.value());
   const response = await fetch(apiRequest);
   const data = await response.json();
 
@@ -28,22 +28,31 @@ const getData = async () => {
   }
 }
 
-let comentariosAnalizados = [];
+function fillLink(id){
+  apiRequest = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId='+id+'&key=AIzaSyDZ9MqeC6ebJXBJNY5X1kACBpHCmUaJtCM&order=relevance';
+}
 
-async function setup() {
+function setup() {
   noCanvas();
   sentiment = ml5.sentiment('movieReviews', modelReady);
   statusEl = createP('Loading Model...');
+  inputLink = createInput('Id de video');
+  videoId = inputLink.value;
+  calcularButton=createButton('Cargar video')
+  calcularButton.addClass('btn btn-outline-success text-dark')
+  calcularButton.mousePressed(async function(){
+    try {
+      await getData();
+    } catch (error) {
+      console.log(error);
+    }
+  })
   submitButton = createButton('Obtener puntajes');
   submitButton.mousePressed(analizeCommentsSentiments);
   submitButton.addClass('btn btn-outline-primary text-dark')
-
+  
   // cargar enseguida los comentarios para evitar problemas de asincronia
-  try {
-    await getData();
-  } catch (error) {
-    console.log(error);
-  }
+
 }
 
 function analizeCommentsSentiments() {
@@ -79,17 +88,23 @@ function draw() {
 function createCard(comment, score) {
   let card = createDiv('');
   pickColor(card, score);
-  let commentText = createP(comment);
-  commentText.parent(card);
   let scoreText = createP(`Puntaje: ${score}`);
   scoreText.parent(card);
+  let commentText = createP(comment);
+  commentText.parent(card);
   card.parent("#cardRow")
 }
 
 function pickColor(card, score) {
-  if (score > 0.5) {
-    card.addClass('card col-2 btn btn-outline-success');
-  } else {
-    card.addClass('card col-2 btn btn-outline-danger')
+  if (score > 0.8) {
+    card.addClass('card col-2 btn btn-success overflow-auto h-25 text-success bg-success');
+  } if(score<0.2) {
+    card.addClass(' overflow-auto card col-2 h-25 btn btn-danger text-danger bg-danger')
+  }if(score>=0.21 && score<=0.4){
+    card.addClass('card col-2 btn btn-info overflow-auto h-25 text-info bg-info');
+  }if(score>=0.4 && score<=0.6){
+    card.addClass('card col-2 btn btn-primary overflow-auto h-25 text-primary bg-primary');
+  }else{
+    card.addClass('card col-2 btn btn-secondary overflow-auto h-25 text-secondary bg-secondary');
   }
 }
